7167d04192aab0b3b762c1754f948bfeaa085431
Henry <Henries90@gmail.com>
2026-04-03 17:36:21 +0100

    Added type safety for window localStorage

--- client/lib/auth.ts
+++ client/lib/auth.ts
@@ -1,22 +1,22 @@
 const TOKEN_KEY = "recalli_token";
 
 export const setToken = (token: string) => {
-  if (typeof window !== undefined) return;
+  if (typeof window === undefined) return;
   localStorage.setItem(TOKEN_KEY, token);
 };
 
 export const getToken = (): string | null => {
-  if (typeof window !== undefined) return null;
+  if (typeof window === undefined) return null;
   return localStorage.getItem(TOKEN_KEY);
 };
 
 export const removeToken = () => {
-  if (typeof window !== undefined) return;
+  if (typeof window === undefined) return;
 
   localStorage.removeItem(TOKEN_KEY);
 };
 
 export const isAuthenticated = (): boolean => {
-  if (typeof window !== undefined) return false;
+  if (typeof window === undefined) return false;
   return !!getToken();
 };
