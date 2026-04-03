40f2328a6f1e48f112d72601f9c828035279aeed
Henry <Henries90@gmail.com>
2026-04-03 17:08:40 +0100

    Changed whitelist

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.NODE_ENV === 'production' ? [] : '*',
+    origin:
+      process.env.NODE_ENV === 'production'
+        ? ['https://recalli.vercel.app']
+        : '*',
   });
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
